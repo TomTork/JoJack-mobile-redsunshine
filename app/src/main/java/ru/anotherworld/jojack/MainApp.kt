bc3012eba2f965e8671cb7fdaa4a4133152eb128
tomtork <dima26483950@gmail.com>
2024-01-21 14:41:50 +0300

    v. 0.2.4a standard design

--- app/src/main/java/ru/anotherworld/jojack/MainApp.kt
+++ app/src/main/java/ru/anotherworld/jojack/MainApp.kt
@@ -2,19 +2,9 @@ package ru.anotherworld.jojack
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.expandHorizontally
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkHorizontally
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -22,40 +12,23 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.WindowInsetsSides
-import androidx.compose.foundation.layout.add
-import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.exclude
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.union
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
@@ -128,12 +101,8 @@ private fun Content(){
     val nunitoFamily = FontFamily(
         Font(R.font.nunito_semibold600, FontWeight.W600)
     )
-    var isVisible by remember { mutableStateOf(true) }
-    var check by remember { mutableStateOf(0) }
     var contentManager by mutableStateOf(0)
-    var visible by remember {
-        mutableStateOf(true)
-    }
+    var select by remember { mutableStateOf(0) } //0 - Home; 1 - chat; 2 - settings
     Scaffold(
         modifier = Modifier.background(Color.White),
         topBar = {
@@ -214,16 +183,15 @@ private fun Content(){
                             .align(Alignment.CenterHorizontally)
                             .offset(y = 3.dp)) {
                             Icon(painterResource(id = R.drawable.home21), null,
-                                tint = colorResource(id = R.color.text_color),
+                                tint = if(contentManager == 0) colorResource(id = R.color.white) else colorResource(id = R.color.text_color),
                                 modifier = Modifier.align(Alignment.CenterHorizontally))
                             Icon(painterResource(id = R.drawable.home22), null,
-                                tint = colorResource(id = R.color.text_color),
+                                tint = if(contentManager == 0) colorResource(id = R.color.white) else colorResource(id = R.color.text_color),
                                 modifier = Modifier
                                     .align(Alignment.CenterHorizontally)
                                     .offset(y = (-6).dp)
                                     .scale(scaleX = 1.5f, scaleY = 1f))
                         }
-
                     }
                     Text(text = stringResource(id = R.string.home),
                         Modifier
@@ -231,7 +199,7 @@ private fun Content(){
                             .shadow(2.dp)
                             .align(Alignment.CenterHorizontally),
                         fontFamily = nunitoFamily, fontWeight = FontWeight.W600,
-                        color = colorResource(id = R.color.text_color))
+                        color = if(contentManager == 0) colorResource(id = R.color.white) else colorResource(id = R.color.text_color))
                 }
                 Column(verticalArrangement = Arrangement.spacedBy(-10.dp),
                     horizontalAlignment = Alignment.CenterHorizontally,
@@ -241,7 +209,7 @@ private fun Content(){
                     IconButton(onClick = { contentManager = 1 }) {
                         Icon(painterResource(id = R.drawable.comments2), null,
                             modifier = Modifier.align(Alignment.CenterHorizontally),
-                            tint = colorResource(id = R.color.text_color))
+                            tint = if(contentManager == 1) colorResource(id = R.color.white) else colorResource(id = R.color.text_color))
                     }
                     Text(text = stringResource(id = R.string.message),
                         Modifier
@@ -249,7 +217,7 @@ private fun Content(){
                             .shadow(2.dp)
                             .align(Alignment.CenterHorizontally),
                         fontFamily = nunitoFamily, fontWeight = FontWeight.W600,
-                        color = colorResource(id = R.color.text_color))
+                        color = if(contentManager == 1) colorResource(id = R.color.white) else colorResource(id = R.color.text_color))
                 }
                 Column(verticalArrangement = Arrangement.spacedBy(-10.dp),
                     horizontalAlignment = Alignment.CenterHorizontally,
@@ -259,7 +227,7 @@ private fun Content(){
                     IconButton(onClick = { contentManager = 2 }) {
                         Icon(painterResource(id = R.drawable.settings2), null,
                             modifier = Modifier.align(Alignment.CenterHorizontally),
-                            tint = colorResource(id = R.color.text_color))
+                            tint = if(contentManager == 2) colorResource(id = R.color.white) else colorResource(id = R.color.text_color))
                     }
                     Text(text = stringResource(id = R.string.settings),
                         Modifier
@@ -267,22 +235,15 @@ private fun Content(){
                             .shadow(2.dp)
                             .align(Alignment.CenterHorizontally),
                         fontFamily = nunitoFamily, fontWeight = FontWeight.W600,
-                        color = colorResource(id = R.color.text_color))
+                        color = if(contentManager == 2) colorResource(id = R.color.white) else colorResource(id = R.color.text_color))
                 }
             }
         }
     ) {
         when(contentManager){
-            0 -> {
-                isVisible = true
-                when(check){
-                    0 -> NewsPaper()
-                    1 -> Meetings()
-                }
-
-            }
-            1 -> {Messenger(); isVisible = false}
-            2 -> {Account(); isVisible = false}
+            0 -> { NewsPaper() }
+            1 -> { Messenger() }
+            2 -> { Account() }
         }
     }
 }
@@ -330,7 +291,7 @@ private fun NewsPaper(){
 private fun Messenger(){
 //    val scrollState = rememberScrollState()
     Column (modifier = Modifier
-        .padding(top = 50.dp, bottom = 60.dp)
+        .padding(top = 60.dp, bottom = 60.dp)
         .clip(
             RoundedCornerShape(
                 topStart = 30.dp,
@@ -338,7 +299,8 @@ private fun Messenger(){
                 bottomEnd = 0.dp,
                 bottomStart = 0.dp
             )
-        ), verticalArrangement = Arrangement.Center) {
+        )
+        .background(colorResource(id = R.color.background2)), verticalArrangement = Arrangement.Center) {
         val listSize = 20
         LazyColumn{
             items(20){
@@ -353,18 +315,27 @@ private fun Messenger(){
 private fun Account(){
     val context = LocalContext.current
     val scrollState = rememberScrollState()
+    val nunitoFamily = FontFamily(
+        Font(R.font.nunito_semibold600, FontWeight.W600),
+        Font(R.font.nunito_medium500, FontWeight.W500)
+    )
     var checked = remember { mutableStateOf(true) }
-    Column(modifier = Modifier.padding(top = 50.dp, bottom = 60.dp)) {
-        Column(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+    Column(modifier = Modifier
+        .padding(top = 60.dp, bottom = 60.dp)
+        .fillMaxWidth(1f)
+        .fillMaxHeight(1f)
+        .background(colorResource(id = R.color.black2))) {
+        Column(modifier = Modifier
+            .align(Alignment.CenterHorizontally)) {
             Icon(painterResource(id = R.drawable.account_circle), null,
                 modifier = Modifier.size(150.dp))
             Column(modifier= Modifier.align(Alignment.CenterHorizontally)) {
                 Text(text = stringResource(id = R.string.login) + ": ",
-                    style=MaterialTheme.typography.labelLarge)
+                    fontFamily = nunitoFamily, fontWeight = FontWeight.W500)
                 Text(text = stringResource(id = R.string.job) + " " + stringResource(id = R.string.jojack),
-                    style=MaterialTheme.typography.labelLarge)
+                    fontFamily = nunitoFamily, fontWeight = FontWeight.W500)
                 Text(text = stringResource(id = R.string.ID),
-                    style=MaterialTheme.typography.labelLarge)
+                    fontFamily = nunitoFamily, fontWeight = FontWeight.W500)
             }
         }
         Spacer(modifier = Modifier.size(20.dp))
@@ -372,7 +343,7 @@ private fun Account(){
             Row(modifier = Modifier
                 .padding(start = 30.dp, end = 30.dp)
                 .background(
-                    colorResource(id = R.color.background_element),
+                    colorResource(id = R.color.background2),
                     shape = RoundedCornerShape(14.dp)
                 )
                 .height(50.dp)) {
@@ -380,18 +351,19 @@ private fun Account(){
                     .weight(0.8f)
                     .align(Alignment.CenterVertically)
                     .padding(start = 10.dp),
-                    fontWeight = FontWeight.Bold, fontSize = 20.sp,
-                    style=MaterialTheme.typography.bodyLarge)
+                    fontSize = 20.sp,
+                    fontFamily = nunitoFamily, fontWeight = FontWeight.W600)
                 Switch(checked = checked.value, onCheckedChange = { checked.value = it;
                     mDatabase.setTheme(mDatabase.boolToInt(it)); changeTheme(it) },
                     modifier = Modifier.weight(0.2f), colors=SwitchDefaults.colors(
-                        checkedThumbColor = colorResource(id = R.color.cred)
+                        checkedThumbColor = colorResource(id = R.color.message_color),
+                        checkedTrackColor = colorResource(id = R.color.my_message_color)
                     ))
             }
             Row (modifier = Modifier
                 .padding(start = 30.dp, end = 30.dp, top = 10.dp)
                 .background(
-                    colorResource(id = R.color.background_element),
+                    colorResource(id = R.color.background2),
                     shape = RoundedCornerShape(14.dp)
                 )
                 .height(50.dp)
@@ -402,13 +374,13 @@ private fun Account(){
                     .weight(0.8f)
                     .align(Alignment.CenterVertically)
                     .padding(start = 10.dp),
-                    fontWeight = FontWeight.Bold, fontSize = 20.sp,
-                    style=MaterialTheme.typography.bodyLarge)
+                    fontSize = 20.sp,
+                    fontFamily = nunitoFamily, fontWeight = FontWeight.W600)
             }
             Row (modifier = Modifier
                 .padding(start = 30.dp, end = 30.dp, top = 10.dp)
                 .background(
-                    colorResource(id = R.color.background_element),
+                    colorResource(id = R.color.background2),
                     shape = RoundedCornerShape(14.dp)
                 )
                 .height(50.dp)
@@ -417,13 +389,13 @@ private fun Account(){
                     .weight(0.8f)
                     .align(Alignment.CenterVertically)
                     .padding(start = 10.dp),
-                    fontWeight = FontWeight.Bold, fontSize = 20.sp,
-                    style=MaterialTheme.typography.bodyLarge)
+                    fontSize = 20.sp,
+                    fontFamily = nunitoFamily, fontWeight = FontWeight.W600)
             }
             Row (modifier = Modifier
                 .padding(start = 30.dp, end = 30.dp, top = 10.dp)
                 .background(
-                    colorResource(id = R.color.background_element),
+                    colorResource(id = R.color.background2),
                     shape = RoundedCornerShape(14.dp)
                 )
                 .height(50.dp)
@@ -432,16 +404,18 @@ private fun Account(){
                     .weight(0.8f)
                     .align(Alignment.CenterVertically)
                     .padding(start = 10.dp),
-                    fontWeight = FontWeight.Bold, fontSize = 20.sp,
-                    style=MaterialTheme.typography.bodyLarge)
+                    fontSize = 20.sp,
+                    fontFamily = nunitoFamily, fontWeight = FontWeight.W600)
             }
             Row(modifier = Modifier
                 .align(Alignment.End)
                 .padding(top = 10.dp, end = 30.dp)) {
-                Button(onClick = { mDatabase.collapseDatabase(); context.startActivity(Intent(context, LoginActivity::class.java)) }) {
-                    Text(text = stringResource(id = R.string.exit), fontWeight = FontWeight.Bold,
+                Button(onClick = { mDatabase.collapseDatabase(); context.startActivity(Intent(context, LoginActivity::class.java)) },
+                    colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.background2))) {
+                    Text(text = stringResource(id = R.string.exit),
                         fontSize = 20.sp, style=MaterialTheme.typography.bodyLarge,
-                        color=colorResource(id=R.color.cred))
+                        color=colorResource(id=R.color.cred),
+                        fontFamily = nunitoFamily, fontWeight = FontWeight.W600)
                 }
             }
         }
--- app/src/main/java/ru/anotherworld/jojack/elements/ChatMessage.kt
+++ app/src/main/java/ru/anotherworld/jojack/elements/ChatMessage.kt
@@ -4,6 +4,7 @@ import android.graphics.Bitmap
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -20,6 +21,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,10 +31,16 @@ import ru.anotherworld.jojack.R
 @Composable
 fun ChatMessage(name: String, previewMessage: String, username: String, image: ImageBitmap? = null,
                 countMessage: Int = 0) {
+    val nunitoFamily = FontFamily(
+        Font(R.font.nunito_semibold600, FontWeight.W600),
+        Font(R.font.nunito_medium500, FontWeight.W500)
+    )
     Column(modifier = Modifier
-        .padding(start = 5.dp, end = 5.dp, top = 5.dp)
-        .fillMaxWidth(1f)) {
-        Divider(color = Color.White, thickness = 1.dp, modifier = Modifier.alpha(0.2f))
+        .padding(top = 4.dp)
+        .fillMaxWidth(1f)
+        .background(colorResource(id = R.color.black2))) {
+        Divider(color = colorResource(id = R.color.background2),
+            thickness = 1.dp, modifier = Modifier.alpha(0.2f))
         Row {
             if(image == null){
                 Icon(
@@ -45,18 +54,19 @@ fun ChatMessage(name: String, previewMessage: String, username: String, image: I
                     .size(40.dp)
                     .padding(start = 10.dp))
             }
-            Text(text = name, fontWeight = FontWeight.ExtraBold, fontSize = 15.sp,
+            Text(text = name, fontWeight = FontWeight.W600, fontSize = 15.sp,
+                fontFamily = nunitoFamily,
                 modifier = Modifier
                     .align(Alignment.CenterVertically)
                     .padding(start = 5.dp), style=MaterialTheme.typography.titleLarge)
             if (countMessage != 0){
                 Text(text = countMessage.toString(), modifier = Modifier
-                    .background(colorResource(id = R.color.grey), shape= RoundedCornerShape(20.dp))
+                    .background(colorResource(id = R.color.grey), shape = RoundedCornerShape(20.dp))
                     .align(Alignment.CenterVertically))
             }
         }
         Text(text = "$username: $previewMessage", modifier = Modifier.padding(start=10.dp,
-            bottom = 5.dp), style=MaterialTheme.typography.bodyLarge)
-
+            bottom = 5.dp), fontFamily = nunitoFamily, fontWeight = FontWeight.W500)
+        Spacer(modifier = Modifier.padding(bottom=4.dp))
     }
 }
