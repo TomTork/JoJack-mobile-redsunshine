3cc55e817957ff810ed7206ee559e3ad3ddf8b2b
tomtork <dima26483950@gmail.com>
2024-03-15 18:54:43 +0300

    v. 0.2.44a удаление чатов + продолжение работы с шифрованием

--- app/src/main/java/ru/anotherworld/jojack/MainApp.kt
+++ app/src/main/java/ru/anotherworld/jojack/MainApp.kt
@@ -27,7 +27,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
